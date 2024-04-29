df9ceea59303c1a4be3b841ebabdb12f37261090
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-04-29 00:33:12 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+document.getElementById('entryForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Get user input
+    var age = parseInt(document.getElementById('age').value);
+    var dayOfWeek = document.getElementById('dayOfWeek').value;
+
+    // Check museum entry conditions
+    var result = "";
+    if (age < 5 || age > 95) {
+        result = "You get in for FREE!";
+    } else if ((age >= 12 && age <= 21) || dayOfWeek === "Tuesday" || dayOfWeek === "Thursday") {
+        result = "You get a DISCOUNT (student price).";
+    } else {
+        result = "You have to pay REGULAR price.";
+    }
+
+    // Display result
+    document.getElementById('result').textContent = result;
+});
